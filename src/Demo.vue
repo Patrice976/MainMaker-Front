cac3cd183444b041c63aa3bc8b447c23b45ccf18
Patrice976 <catinatp@gmail.com>
2025-02-06 11:38:17 +0100

    création répo (Docker disponible)

---
+++ src/Demo.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+withDefaults(
+    defineProps<{
+    prefix : string,
+    person: {
+        firstname : string, 
+        lastname : string
+    }
+}>(), {
+    prefix : 'Bonjour'
+}
+)
+
+</script>
+
+<template>
+
+</template>
