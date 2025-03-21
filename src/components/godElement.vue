0cb72b9f4c400bd5f455afd8c25dd0aafd650b2a
Patrice976 <catinatp@gmail.com>
2025-03-22 00:44:08 +0100

    récupération image + début mise en page

--- src/components/godElement.vue
+++ src/components/godElement.vue
@@ -1,12 +1,40 @@
 
 <template>
-
+<div class="hexagone">
+  <img :src="godData.imageUrl" :alt="godData.name" />
+</div>
 </template>
 
 <script setup lang="ts">
+import {defineProps} from 'vue'
+
+defineProps({
+  godData : {
+    type: Object,
+    required: true,
+  }
+})
 
 </script>
 
 <style scoped>
-
+.hexagon {
+  width: 100px;
+  height: 100px;
+  clip-path: polygon(
+    50% 0%,
+    100% 25%,
+    100% 75%,
+    50% 100%,
+    0% 75%,
+    0% 25%
+  );
+  overflow: hidden;
+  cursor: pointer;
+}
+.hexagon img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,12 +4,20 @@
   <input type="text" placeholder="Cherchez votre divinité">
   <button>Go!</button>
 </div>
+  <div class ="listgod">
+  <GodElement
+      v-for="(god, index) in gods"
+      :key="index"
+      :godData="god"
+    />
+</div>
 
 
 </template>
 
 <script setup lang="ts">
 import {ref, onMounted} from 'vue'
+import GodElement from "../components/godElement.vue"
 
 const gods =ref([])
 
@@ -18,7 +26,7 @@ onMounted(async () => {
     //On va chercher dans l'API
     const resp = await fetch('http://localhost:8000/api/champions')
     gods.value = await resp.json()
-    console.log(gods.value)
+    console.log(gods.value[0])
   } catch (error) {
     console.error("Erreur lors du fetch :", error)
   }
@@ -28,5 +36,8 @@ onMounted(async () => {
 </script>
 
 <style scoped>
-
+.listgod {
+  display: flex ;
+  align-items: center;
+}
 </style>
